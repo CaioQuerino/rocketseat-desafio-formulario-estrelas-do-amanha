426067f8bf99d08737939d246f8696c6a29bf854
CaioQuerino <caioquerino04@gmail.com>
2024-08-17 19:02:33 -0300

    17-08-2024 : Alteração do nome da pasta dos icons e adicionando os icones de calendário e arrow down aos seus respectivos campos.

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const formatarTelefone = (input) => {
+    let tel = input.value.replace(/\D/g, '');
+
+    if (tel.length > 14) tel = tel.slice(0, 14);
+
+    tel = tel.replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3');
+    input.value = tel;
+}
+
+function validarEmail(input) {
+    const dominio = '@rocketseat.com.br';
+    let valor = input.value;
+
+    valor = valor.replace(/[^a-zA-Z0-9._-]/g, '').trim();
+
+    const emailPattern = /^[a-zA-Z0-9._-]+$/;
+
+    if (emailPattern.test(valor)) {
+        input.value = valor + dominio;
+    } else {
+        input.value = valor;
+    }
+}
